e6b5a6bd8be205db2525586da0b521584cafb138
Gilber Alejandro Martinez Franco <gilber_martinez@soy.sena.edu.co>
2025-10-22 14:21:10 -0500

    Listo

--- backend/src/models/Investment.js
+++
@@ -1,9 +0,0 @@
-class Investment {
-	constructor({ id, name, type, riskRating }) {
-		this.id = id;
-		this.name = name;
-		this.type = type;
-		this.riskRating = riskRating;
-	}
-}
-module.exports = Investment;
