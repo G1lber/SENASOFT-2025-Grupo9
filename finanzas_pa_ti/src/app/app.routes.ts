09f9d1d5deca146be1053b40dcfed57f499ea527
Gysel Mariana Rodriguez <gyselmarianarodriguezquincoz@gmail.com>
2025-10-22 12:33:02 -0500

    add interface login and configuration

--- finanzas_pa_ti/src/app/app.routes.ts
+++ finanzas_pa_ti/src/app/app.routes.ts
@@ -1,24 +1,27 @@
-import {Routes} from '@angular/router';
-import {Contenido} from './components/contenido/contenido';
-import {Header} from './components/header/header';
-import {Chat} from './components/chat/chat';
+import { Routes } from '@angular/router';
+import { Contenido } from './components/contenido/contenido';
+import { Login } from './components/login/login';
+import { Chat } from './components/chat/chat';
 
-function authGuard() {
-
+export function authGuard() {
+  return true;
 }
 
 export const routes: Routes = [
-  { path: '',
-  component: Contenido
-},
   {
-    path: 'asesor',
+    path: '',
+    component: Login
+  },
+  {
+    path: 'inicio',
     component: Contenido,
     canActivate: [authGuard],
     children: [
-      {path: 'asesor', component: Chat},
-
+      { path: 'asesor', component: Chat }
     ]
   },
-  {path: '**', redirectTo: ''}
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
--- finanzas_pa_ti/src/app/components/contenido/contenido.ts
+++ finanzas_pa_ti/src/app/components/contenido/contenido.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Header} from '../header/header';
 import {RouterOutlet} from '@angular/router';
+import {Footer} from '../footer/footer';
 
 @Component({
   selector: 'app-contenido',
   imports: [
     Header,
-    RouterOutlet
+    RouterOutlet,
+    Footer
   ],
   templateUrl: './contenido.html',
   styleUrl: './contenido.css'
