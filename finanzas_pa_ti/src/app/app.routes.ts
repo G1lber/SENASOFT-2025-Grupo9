5c7a9bffdb4be6e5300e741e6eb52642629b2f9d
MarianaRQ <gyselmarianarodriguezquincoz@gmail.com>
2025-10-21 23:30:57 -0500

    add interface header, contenido, chat

--- finanzas_pa_ti/src/app/app.routes.ts
+++ finanzas_pa_ti/src/app/app.routes.ts
@@ -1,5 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Contenido} from './components/contenido/contenido';
+import {Header} from './components/header/header';
+import {Chat} from './components/chat/chat';
+
+function authGuard() {
+
+}
 
 export const routes: Routes = [
+  { path: '',
+  component: Contenido
+},
+  {
+    path: 'asesor',
+    component: Contenido,
+    canActivate: [authGuard],
+    children: [
+      {path: 'asesor', component: Chat},
 
+    ]
+  },
+  {path: '**', redirectTo: ''}
 ];
--- finanzas_pa_ti/src/app/components/contenido/contenido.ts
+++ finanzas_pa_ti/src/app/components/contenido/contenido.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {Header} from '../header/header';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-contenido',
-  imports: [],
+  imports: [
+    Header,
+    RouterOutlet
+  ],
   templateUrl: './contenido.html',
   styleUrl: './contenido.css'
 })
